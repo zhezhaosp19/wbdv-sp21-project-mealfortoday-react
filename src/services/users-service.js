3d5634f898cbf19abca0d1cb5bb9d0ede4599009
zhezhaosp19 <zhe_zhao@berkeley.edu>
2021-04-06 14:46:00 +0800

    update users service

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -10,15 +10,6 @@ export const createUsers = (user) =>
         }
     }).then(response => response.json())
 
-export const createCourse = (user) =>
-    fetch(REGISTER_URL, {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-            'content-type': 'application/json'
-        }
-    }).then(response => response.json())
-
 export default {
     createUsers
 }
