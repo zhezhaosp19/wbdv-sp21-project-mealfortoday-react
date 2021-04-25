ca4de0cf230ca3a3ee92701f36f98cd8ee2de16d
zhezhaosp19 <zhe_zhao@berkeley.edu>
2021-04-25 23:35:14 +0800

    upddate profile

--- src/components/cookie/profile.js
+++ src/components/cookie/profile.js
@@ -18,68 +18,72 @@ const Profile = () => {
             <Header/>
             <div className="container profile">
 
-                {
-                    currentUser && username !== "undefined" &&
-                    <>
-                        <div style={{display: 'flex', justifyContent: "space-around", margin: '18px 0px'}}>
-                            <div>
-                                <img className="image"
-                                     src="https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"/>
-                            </div>
-                            <div>
-                                <div style={{display: 'flex', justifyContent: "space-between", width: "150%"}}>
-                                    <h2>{currentUser.username}</h2>
-                                    {currentUser.username === username &&
-                                    <div className="float-right">
-                                        <Link to={`/update-profile/${currentUser.username}`} className="btn edit-profile-button">Edit Profile</Link>
-                                    </div>
-                                    }
-                                </div>
+                <div style={{display: 'flex', justifyContent: "space-around", margin: '18px 0px'}}>
+                    <div>
+                        <img className="image"
+                             src="https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"/>
+                    </div>
+                    <div>
+                        <div style={{display: 'flex', justifyContent: "space-between", width: "150%"}}>
+                            {
+                                currentUser.username === username &&
+                                <h2>{currentUser.username}</h2>
+                            }
+                            {
+                                currentUser.username !== username &&
+                                <h2>{username}</h2>
+                            }
 
-                                <div style={{display: 'flex', marginTop: 20, justifyContent: "space-between", width: "150%"}}>
-                                    {currentUser.role === "Chef" &&
-                                    <>
-                                        <h5>5 posts</h5>
-                                    </>
-                                    }
-                                    <h5>5 followers</h5>
-                                    <h5>5 followings</h5>
+                            {currentUser.username === username &&
+                                <div className="float-right">
+                                    <Link to={`/update-profile/${currentUser.username}`} className="btn edit-profile-button">Edit Profile</Link>
                                 </div>
-                                <div>
-                                    <p style={{fontSize: 20, marginTop: 10}}>Bio</p>
-                                    <a href="https://www.google.com">https://www.google.com</a>
-                                </div>
-                            </div>
+                            }
                         </div>
+
+                        {/*<div style={{display: 'flex', marginTop: 20, justifyContent: "space-between", width: "150%"}}>*/}
+                        {/*    {currentUser.role === "Chef" &&*/}
+                        {/*    <>*/}
+                        {/*        <h5>5 posts</h5>*/}
+                        {/*    </>*/}
+                        {/*    }*/}
+                        {/*    <h5>5 followers</h5>*/}
+                        {/*    <h5>5 followings</h5>*/}
+                        {/*</div>*/}
                         <div>
-                            <div className="cell-box">
-                                <div className="form-group row cell">
-                                    {
-                                        currentUser.role === "Chef" &&
-                                        <>
-                                            <div className="col-6 each-cell">
-                                                Your posts
-                                            </div>
-                                            <div className="col-6 each-cell">
-                                                Your Favorites
-                                            </div>
-                                        </>
-                                    }
-                                    {
-                                        currentUser.role !== "Chef" &&
-                                        <>
-                                            <div className="col-12 each-cell">
-                                                Your Favorites
-                                            </div>
-                                        </>
-                                    }
-
-
-                                </div>
-                            </div>
+                            <p style={{fontSize: 20, marginTop: 10}}>Bio</p>
+                            <a href="https://www.google.com">https://www.google.com</a>
                         </div>
-                    </>
-                    }
+                    </div>
+                </div>
+                <div>
+                    <div className="cell-box">
+                        <div className="form-group row cell">
+                            {
+                                currentUser.role === "Chef" &&
+                                <>
+                                    <div className="col-6 each-cell">
+                                        Your posts
+                                    </div>
+                                    <div className="col-6 each-cell">
+                                        Your Favorites
+                                    </div>
+                                </>
+                            }
+                            {
+                                currentUser.role !== "Chef" &&
+                                <>
+                                    <div className="col-12 each-cell">
+                                        Your Favorites
+                                    </div>
+                                </>
+                            }
+
+
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </>
