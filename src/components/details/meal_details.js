51d19f0bc0cec739e49213298cf9b6ed4080fec0
Namuhan Muschin <namuhan@Namuhans-MacBook-Pro.local>
2021-04-25 14:44:53 -0700

    Added favorite ui

--- src/components/details/meal_details.js
+++ src/components/details/meal_details.js
@@ -17,6 +17,7 @@ const MealDetails = () => {
   const [meal, setMeal] = useState({})
   const {idMeal, searchTitle} = useParams();
   const [results, setResults] = useState({meals:[]})
+  const [isFavorite, setIsFavorite] = useState(false)
   const [similarDishesCount, setSimilarDishesCount] = useState(0)
 
   useEffect(() => {
@@ -34,16 +35,27 @@ const MealDetails = () => {
     .then((meal) => setMeal(meal.meals[0]))
   }
 
+  const setFavorite = (set) => {
+    if (set) {
+      setIsFavorite(true)
+    } else {
+      setIsFavorite(false)
+    }
+  }
+
   return (
     <>
       <Header/>
       <div className="container mt-5">
         <button className="btn btn-primary" onClick={()=>{history.goBack()}}>Back</button>
         <h1>{meal.strMeal}</h1>
-        <i class="bi bi-star"></i>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-          <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-        </svg>
+        <div>
+          Posted by
+          <Link to="/profile/username"> user</Link>
+        </div>
+        {!isFavorite && <i onClick={() => setFavorite(true)} className="far fa-star"></i>}
+        {isFavorite && <i onClick={() => setFavorite(false)} className="fas fa-star"></i>}
+
         <img className="mealThumb" src={meal.strMealThumb} width={500}/>
 
         {showBriefInfo(meal)}
@@ -57,8 +69,8 @@ const MealDetails = () => {
         <h2>Reviews</h2>
         {reviews(meal)}
 
-        <h2>Similar Dishes</h2>
-        {displaySimilarDishes(similarDishesCount, results, setSimilarDishesCount, searchTitle, meal)}
+        {/*<h2>Similar Dishes</h2>*/}
+        {/*{displaySimilarDishes(similarDishesCount, results, setSimilarDishesCount, searchTitle, meal)}*/}
 
         <br/>
         {/*{JSON.stringify(results)}*/}
